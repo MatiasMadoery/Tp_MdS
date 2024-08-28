5c8a896a661ecfc260d4565e54ec7b9881d93d98
MatiasMadoery <matimadoery@hotmail.com>
2024-08-28 10:41:58 -0300

    Issue1: Crear el modelo básico de usuario v2

--- src/main/kotlin/utn/methodology/domain/entities/User.kt
+++ src/main/kotlin/utn/methodology/domain/entities/User.kt
@@ -1,15 +1,49 @@
 package utn.methodology.domain.entities
 import ch.qos.logback.core.status.Status
-import com.fasterxml.jackson.databind.ser.std.UUIDSerializer
-import java.util.UUID
-
 
 data class User (
-    val id : Int,
-    val name : String,
-    val username : String,
-    val email : String,
-    val password : String
+    private val id : String,
+    private var name : String,
+    private var username : String,
+    private var email : String,
+    private var password : String
 )
+
+{
+    companion object {
+        fun fromPrimitives(primitives: Map<String, String>): User{
+            val user = User(
+                primitives["id"] as String,
+                primitives["name"] as String,
+                primitives["username"] as String,
+                primitives["email"] as String,
+                primitives["password"] as String
+            );
+            return user;
+        }
+    }
+
+    fun getId(): String{
+        return this.id;
+    }
+
+    fun update(name: String, username: String, email: String, password: String){
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    fun toPrimitives(): Map<String, String>{
+        return mapOf(
+            "id" to this.id,
+            "name" to this.name,
+            "username" to this.username,
+            "email" to  this.email,
+            "password" to this.password
+        )
+    }
+}
+
 
 
