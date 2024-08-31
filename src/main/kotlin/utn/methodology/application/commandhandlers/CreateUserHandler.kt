36b01e7158d4f5bc22f5cd655ff0fdc5254aa068
victor caula <victor21gabriel2002@gmail.com>
2024-08-30 21:53:08 -0300

    Issue 4: correction name of variables

--- src/main/kotlin/utn/methodology/application/commandhandlers/CreateUserHandler.kt
+++ src/main/kotlin/utn/methodology/application/commandhandlers/CreateUserHandler.kt
@@ -1,11 +1,12 @@
 package utn.methodology.application.commandhandlers
 
+import com.mongodb.client.MongoDatabase
 import utn.methodology.application.commands.CreateUserCommand
 import java.util.UUID
 import utn.methodology.domain.entities.User
-import utn.methodology.infrastructure.persistence.repositories.UserRepository
+import utn.methodology.infrastructure.persistence.repositories.UserRepository // aun no esta bien creado va Mongo
 
-class ConfirmHandler (
+class CreateUserHandler (
             private val userRepository: UserRepository,
     )
 {
@@ -13,14 +14,13 @@ class ConfirmHandler (
     {
         val user = User.handle(
             UUID.randomUUID().toString(),
-            command.Id,
             command.Name,
             command.Username,
             command.Email,
             command.Password,
         )
 
-        userRepository.Update(User)
+        userRepository.Save(user)
     }
 
 }
