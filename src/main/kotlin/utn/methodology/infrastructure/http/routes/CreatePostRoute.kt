768e4f1c2509ab6aceaad435339f5cdbb2974ce3
MatiasMadoery <matimadoery@hotmail.com>
2024-09-20 21:34:47 -0300

    Issue8 v_2

--- src/main/kotlin/utn/methodology/infrastructure/http/routes/CreatePostRoute.kt
+++ src/main/kotlin/utn/methodology/infrastructure/http/routes/CreatePostRoute.kt
@@ -6,18 +6,20 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import utn.methodology.infrastructure.persistence.connectToMongoDB
+import utn.methodology.infrastructure.persistence.repositories.UserRepository
 
 
-fun Application.createPostRoutes() {
+fun Application.postRoutes() {
     val mongoDatabase = connectToMongoDB()
-    val createPostAction = CreatePostAction(CreatePostHandler(CreatePostRepository))
+    val postRepository = PostRepository(mongoDatabase)
+    val postAction = PostAction(PostHandler(postRepository))
 
     routing {
         post("/posts") {
-            val body = call.receive<CreatePostCommand>()
+            val body = call.receive<PostCommand>()
             try {
                 if (body.message.Length > 100) {
-                    createPostAction.execute(body)
+                    postAction.execute(body)
                     call.respond(HttpStatusCode.Created, mapOf("message" to "Entry too long!"))
                 }
             } catch (e: Exception) {
