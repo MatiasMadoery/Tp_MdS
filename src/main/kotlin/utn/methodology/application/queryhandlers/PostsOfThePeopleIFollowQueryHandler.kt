076ff6593d26ea268b64ca3a85aee3c3d9265f91
victor caula <victor21gabriel2002@gmail.com>
2024-09-20 21:41:21 -0300

    Issue12

---
+++ src/main/kotlin/utn/methodology/application/queryhandlers/PostsOfThePeopleIFollowQueryHandler.kt
@@ -0,0 +1,13 @@
+package utn.methodology.application.queryhandlers
+
+import utn.methodology.application.queries.PostsOfThePeopleIFollowQuery
+import utn.methodology.infrastructure.persistence.repositories.PostRepository
+
+class PostsOfThePeopleIFollowQueryHandler (private val postRepository: PostRepository) {
+    fun handle(query: PostsOfThePeopleIFollowQuery): List<Post> {
+        val idsFollowers = postRepository.GetTrackedIds(query.UserId)
+
+        return postRepository.getPostsByUsers(idsFollowers)
+    }
+
+}
---
+++ src/main/kotlin/utn/methodology/infrastructure/http/actions/PostOfThePeopleIFolloweAction.kt
@@ -0,0 +1,16 @@
+package utn.methodology.infrastructure.http.actions
+
+import utn.methodology.application.queries.PostsOfThePeopleIFollowQuery
+import utn.methodology.application.queryhandlers.PostsOfThePeopleIFollowQueryHandler
+
+class PostsOfThePeopleIFollowAction (private val queryHandler: PostsOfThePeopleIFollowQueryHandler){
+    fun execute(UserId: String): List<Post> {
+        val query = PostsOfThePeopleIFollowQuery(UserId)
+        return queryHandler.handle(query)
+    }
+}
+//esto es dentro del repositorio son los datos que necesito solicitar
+interface PostRepository {
+    fun obtenerIdsSeguidos(usuarioId: String): List<String>
+    fun obtenerPosteosPorUsuarios(ids: List<String>): List<Post>
+}
