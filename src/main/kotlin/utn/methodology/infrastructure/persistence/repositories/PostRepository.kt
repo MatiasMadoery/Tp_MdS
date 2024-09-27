2b3ef1e8a30f1b4c2c2ba89af13bf9241a4e2a59
Ignacio Merlo <nachoamerlo@gmail.com>
2024-09-27 15:42:52 -0300

    Issue9 v1

---
+++ src/main/kotlin/utn/methodology/infrastructure/persistence/repositories/PostRepository.kt
@@ -0,0 +1,41 @@
+package utn.methodology.infrastructure.persistence.repositories
+
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+import utn.methodology.domain.entities.Post
+
+interface PostRepositoryInterface {
+    suspend fun getPostsByUserId(userId: String, order: String, limit: Int, offset: Int): List<Post>
+}
+
+class PostRepository (private val database: MongoDatabase) : PostRepositoryInterface {
+    private var collection: MongoCollection<Document> = database.getCollection("posts") as MongoCollection<Document>
+
+    override suspend fun getPostsByUserId(
+        userId: String,
+        order: String,
+        limit: Int,
+        offset: Int
+    ): List<Post> {
+        val sortOrder = if (order == "ASC") 1 else -1
+
+        val query = Document("userId", userId)
+        val sort = Document("date", sortOrder)
+
+        return collection.find(query)
+            .sort(sort)
+            .skip(offset)
+            .limit(limit)
+            .map { documentToPost(it) }
+            .toList()
+    }
+
+    private fun documentToPost(document: Document): Post {
+        return Post(
+            userId = document.getString("userId"),
+            message = document.getString("message"),
+            date = document.getDate("date")
+        )
+    }
+}
